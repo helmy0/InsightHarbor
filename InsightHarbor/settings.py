0aef0fbaa50f09548ba77948199d2dc944feec49
Omar Farrag <omar.tarek.farrag123@gmail.com>
2024-03-04 22:22:05 +0000

    fixed secuirty issues

--- InsightHarbor/settings.py
+++ InsightHarbor/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 from django.contrib import staticfiles
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x$c$p4od8o6jy8rln7da%8&r_0jsoy7n0pugesri=g=k@pmj=d"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
