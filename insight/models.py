9a38831cc50bec76467c6142dcb9707557120dce
Omar Farrag <omar.tarek.farrag123@gmail.com>
2024-03-07 16:36:50 +0000

    added model UserImageCase

--- insight/models.py
+++ insight/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class UserImageCase(models.Model):
+    image = models.ImageField()
+
+    userPrompt = models.TextField()
+
+    dateCreated = models.DateField().auto_now_add
+
+    resultPrompt = models.TextField()
