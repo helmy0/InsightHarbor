3159931793eb0c5aa2332403d580c0ad8434143b
Omar Farrag <omar.tarek.farrag123@gmail.com>
2024-06-01 00:40:43 +0300

    added allowed hosts

--- InsightHarbor/settings.py
+++ InsightHarbor/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "insightharbor.onrender.com",
+]
 
 
 # Application definition
